d0b179c280255bb540bdce59e0164bdc39c504ff
Al Ramsey <aramsey@capsci.com>
2018-03-30 09:08:36 -0400

    Updated with new tiles

--- client/js/index.js
+++ client/js/index.js
@@ -1,14 +1,14 @@
 var map = new ol.Map({
     layers: [
         new ol.layer.Tile({
-                source: new ol.source.XYZ({
+            source: new ol.source.XYZ({
                 url: "http://localhost:3000/tiles/{z}/{x}/{y}.png"
             })
         })
     ],
     target: 'map',
     view: new ol.View({
-        center: [0, 0],
-        zoom: 2
+        center: [40, -70],
+        zoom: 0
     })
 });
